e5d255d2104b286ad85d7bb028a1ddf51595df4f
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-06-11 14:09:27 -0400

    first commit

---
+++ src/main/java/com/example/spotifyscrobble/SpotifyScrobbleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyscrobble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyScrobbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyScrobbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotifyscrobble/SpotifyScrobbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyscrobble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyScrobbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
