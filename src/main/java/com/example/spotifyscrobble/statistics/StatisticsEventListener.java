a66ae46f86754bb9fec4101acaa2b94bbf1fcaab
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-06-20 17:57:10 -0400

    updated some function names and removed previous print statements for basic debugging (im pretty sure they arent even in the previous commit but hwatever) and updated some simple code

--- src/main/java/com/example/spotifyscrobble/statistics/StatisticsEventListener.java
+++ src/main/java/com/example/spotifyscrobble/statistics/StatisticsEventListener.java
@@ -4,12 +4,6 @@ import com.example.spotifyscrobble.listening.TrackListenedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
-import com.example.spotifyscrobble.statistics.entity.ArtistStatsEntity;
-import com.example.spotifyscrobble.statistics.entity.TrackStatsEntity;
-import com.example.spotifyscrobble.statistics.repositories.ArtistStatsRepository;
-import com.example.spotifyscrobble.statistics.repositories.TrackStatsRepository;
-import com.example.spotifyscrobble.users.UserRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Slf4j
--- src/main/java/com/example/spotifyscrobble/statistics/StatisticsService.java
+++ src/main/java/com/example/spotifyscrobble/statistics/StatisticsService.java
@@ -8,7 +8,6 @@ import com.example.spotifyscrobble.listening.TrackListenedEvent;
 import com.example.spotifyscrobble.users.UserEntity;
 import com.example.spotifyscrobble.users.UserRepository;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import com.example.spotifyscrobble.statistics.entity.ArtistStatsEntity;
 import com.example.spotifyscrobble.statistics.entity.TrackStatsEntity;
@@ -35,28 +34,26 @@ public class StatisticsService {
     @Transactional
     public void handleTrackListenedEvent(TrackListenedEvent event){
         CatalogEntriesExistResponse response = catalogService.getCatalogEntries(event.trackId(), event.artistId());
-        createNewArtistStats(response.artist());
-        createNewTrackStats(response.track());
+        initializeNewArtistStats(response.artist());
+        initializeNewTrackStats(response.track());
+
         UserEntity user = userRepo.findById(event.userId()).orElseThrow(() -> new IllegalArgumentException("User does not exist"));
         if(!isExistingArtistListener(response.artist(), user)){
-            System.out.println("HI from if statement");
             artistStatsRepo.incrementListeners(response.artist().getArtistId());
         };
 
-        System.out.println("HI5");
         artistStatsRepo.incrementTotalPlays(response.artist().getArtistId());
-        System.out.println("HI6");
         trackStatsRepo.incrementTrackPlays(response.track().getTrackId());
 
     }
 
-    public void createNewArtistStats(ArtistEntity artist){
+    public void initializeNewArtistStats(ArtistEntity artist){
         if (!artistStatsRepo.existsById(artist.getArtistId())) {
             artistStatsRepo.save(new ArtistStatsEntity(artist));
         }
     }
 
-    public void createNewTrackStats(TrackEntity track){
+    public void initializeNewTrackStats(TrackEntity track){
         if (!trackStatsRepo.existsById(track.getTrackId())) {
             trackStatsRepo.save(new TrackStatsEntity(track));
         }
@@ -73,5 +70,4 @@ public class StatisticsService {
             return true;
         }
     }
-
 }
