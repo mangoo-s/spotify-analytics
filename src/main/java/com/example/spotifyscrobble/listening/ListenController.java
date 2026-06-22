72bf852c77f9520a173b673e86df9d3448a304b7
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-06-22 13:06:50 -0400

    Created get endpoint to see listening history and ensured custom pagination to stop response from being verbose

--- src/main/java/com/example/spotifyscrobble/listening/ListenController.java
+++ src/main/java/com/example/spotifyscrobble/listening/ListenController.java
@@ -1,8 +1,13 @@
 package com.example.spotifyscrobble.listening;
 
+import com.example.spotifyscrobble.shared.CustomPageResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.security.auth.login.AccountNotFoundException;
@@ -16,6 +21,11 @@ public class ListenController {
         this.listeningService = listeningService;
     }
 
+    @GetMapping("/users/{id}/history")
+    public ResponseEntity<?> getListeningHistory(@PathVariable Long id, Pageable p){
+        CustomPageResponse<ListeningHistoryResponse> response = listeningService.getUserListeningHistory(id, p);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
     @GetMapping("/test")
     public ResponseEntity<?> test() throws AccountNotFoundException {
