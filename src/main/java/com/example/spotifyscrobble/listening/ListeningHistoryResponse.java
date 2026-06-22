4f39bc8547f1197d5c55c091b82ac4289fcfdfbb
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-06-22 13:07:54 -0400

    created listeninghistoryresponse for listenevententity and to only pass on values we need

---
+++ src/main/java/com/example/spotifyscrobble/listening/ListeningHistoryResponse.java
@@ -0,0 +1,13 @@
+package com.example.spotifyscrobble.listening;
+
+import java.time.Instant;
+
+
+public record ListeningHistoryResponse(
+        String artistName,
+        String trackName,
+        Long spotifyArtistId,
+        Long spotifyTrackId,
+        Instant playedAt
+) {
+}
