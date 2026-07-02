6c9c2558dd10d1fd6082f82027ce573bfb1bf31f
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-07-02 19:07:31 -0400

    added top artists endpoint

--- src/main/java/com/example/spotifyscrobble/statistics/controller/StatisticsController.java
+++ src/main/java/com/example/spotifyscrobble/statistics/controller/StatisticsController.java
@@ -1,6 +1,7 @@
 package com.example.spotifyscrobble.statistics.controller;
 
 import com.example.spotifyscrobble.shared.CustomPageResponse;
+import com.example.spotifyscrobble.statistics.UserTopArtistResponse;
 import com.example.spotifyscrobble.statistics.UserTopTracksResponse;
 import com.example.spotifyscrobble.statistics.services.StatisticsService;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,14 @@ public class StatisticsController {
                                                                                       @RequestParam(defaultValue = "0") int page,
                                                                                       @RequestParam(defaultValue = "10") int size){
         CustomPageResponse<UserTopTracksResponse> response = statisticsService.getUsersTopTracks(id, page, size);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/{id}/top-artists")
+    public ResponseEntity<CustomPageResponse<UserTopArtistResponse>> getUserTopArtists(@PathVariable long id,
+                                                                                       @RequestParam(defaultValue = "0") int page,
+                                                                                       @RequestParam(defaultValue = "10") int size){
+        CustomPageResponse<UserTopArtistResponse> response = statisticsService.getUsersTopArtists(id, page, size);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
