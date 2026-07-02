00b7912f5d5862564a1e281764824585191e976e
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-07-02 19:08:02 -0400

    New find all by user id function

--- src/main/java/com/example/spotifyscrobble/statistics/repositories/UserArtistStatsRepository.java
+++ src/main/java/com/example/spotifyscrobble/statistics/repositories/UserArtistStatsRepository.java
@@ -2,6 +2,8 @@ package com.example.spotifyscrobble.statistics.repositories;
 
 import com.example.spotifyscrobble.statistics.entities.UserArtistStatsEntity;
 import com.example.spotifyscrobble.statistics.entities.UserArtistStatsId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +17,7 @@ public interface UserArtistStatsRepository extends JpaRepository<UserArtistStats
     AND arts.id.userId = :#{#id.userId}
     """)
     int incrementTotalPlays(UserArtistStatsId id);
+
+    Page<UserArtistStatsEntity> findAllById_UserId(Long userId, Pageable p);
 
 }
