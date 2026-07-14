9128a1fe5fe184a8fb2b0d6d78c5aef5b14f6118
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-07-13 20:51:14 -0400

    Formatted code to be cleaner and simpler as handletracklistenedevent was becoming too much

--- src/main/java/com/example/spotifyscrobble/statistics/services/StatisticsService.java
+++ src/main/java/com/example/spotifyscrobble/statistics/services/StatisticsService.java
@@ -4,11 +4,11 @@ import com.example.spotifyscrobble.catalog.*;
 import com.example.spotifyscrobble.shared.CustomPageResponse;
 import com.example.spotifyscrobble.statistics.UserTopArtistResponse;
 import com.example.spotifyscrobble.statistics.UserTopTracksResponse;
-import com.example.spotifyscrobble.statistics.entities.*;
+import com.example.spotifyscrobble.statistics.components.ArtistStatsUpdater;
+import com.example.spotifyscrobble.statistics.components.TrackStatsUpdater;
+import com.example.spotifyscrobble.statistics.components.UserStatsUpdater;
 import com.example.spotifyscrobble.statistics.repositories.*;
 import com.example.spotifyscrobble.users.UsersApi;
-import com.example.spotifyscrobble.users.entity.UserEntity;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,37 +17,34 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class StatisticsService {
-    private final ArtistStatsRepository artistStatsRepo;
-    private final TrackStatsRepository trackStatsRepo;
-    private final ArtistListenerRepository artistListenerRepo;
     private final UserArtistStatsRepository userArtistStatsRepo;
     private final UserTrackStatsRepository userTrackStatsRepository;
     private final UsersApi usersApi;
+    private final UserStatsUpdater userStatsUpdater;
+    private final ArtistStatsUpdater artistStatsUpdater;
+    private final TrackStatsUpdater trackStatsUpdater;
 
-    public StatisticsService(ArtistStatsRepository artistStatsRepo, TrackStatsRepository trackStatsRepo, ArtistListenerRepository artistListenerRepo, UserArtistStatsRepository userArtistStatsRepo, UserTrackStatsRepository userTrackStatsRepository, UsersApi usersApi) {
-        this.artistStatsRepo = artistStatsRepo;
-        this.trackStatsRepo = trackStatsRepo;
-        this.artistListenerRepo = artistListenerRepo;
+    public StatisticsService(UserArtistStatsRepository userArtistStatsRepo,
+                             UserTrackStatsRepository userTrackStatsRepo,
+                             UsersApi usersApi, UserStatsUpdater userStatsUpdater,
+                             ArtistStatsUpdater artistStatsUpdater,
+                             TrackStatsUpdater trackStatsUpdater) {
+
         this.userArtistStatsRepo = userArtistStatsRepo;
-        this.userTrackStatsRepository = userTrackStatsRepository;
+        this.userTrackStatsRepository = userTrackStatsRepo;
         this.usersApi = usersApi;
+        this.userStatsUpdater = userStatsUpdater;
+        this.artistStatsUpdater = artistStatsUpdater;
+        this.trackStatsUpdater = trackStatsUpdater;
     }
 
+    @Transactional
     public void handleTrackListenedEvent(CatalogEntriesResolvedEvent event){
-        initializeNewArtistStats(event.artistId(), event.artistName());
-        initializeNewTrackStats(event.trackId(), event.trackName());
-
-        initializeNewUserArtistStatsOrIncrement(event.userId(), event.artistId(), event.artistName());
-        initializeNewUserTrackStatsOrIncrement(event.userId(), event.trackId(), event.trackName(), event.artistName());
-
-        if(!isExistingArtistListener(event.artistId(), event.userId())){
-            artistStatsRepo.incrementListeners(event.artistId());
-        }
-        artistStatsRepo.incrementTotalPlays(event.artistId());
-        trackStatsRepo.incrementTrackPlays(event.trackId());
-
+        artistStatsUpdater.recordListen(event.artistId(), event.artistName(), event.userId());
+        trackStatsUpdater.recordPlay(event.trackId(), event.trackName()); // Need to make track listener table
+        userStatsUpdater.recordTrackPlay(event.userId(), event.trackId(), event.trackName(), event.artistName());
+        userStatsUpdater.recordArtistPlay(event.userId(), event.artistId(), event.artistName());
     }
 
     public CustomPageResponse<UserTopTracksResponse> getUsersTopTracks(Long userId, int page, int size){
@@ -71,43 +68,4 @@ public class StatisticsService {
         return new CustomPageResponse<>(artists);
     }
 
-    public void initializeNewArtistStats(Long artistId, String name){
-        if (!artistStatsRepo.existsById(artistId)) {
-            artistStatsRepo.save(new ArtistStatsEntity(artistId, name));
-        }
-    }
-
-    public void initializeNewTrackStats(Long trackId, String title){
-        if (!trackStatsRepo.existsById(trackId)) {
-            trackStatsRepo.save(new TrackStatsEntity(trackId, title));
-        }
-    }
-
-    public boolean isExistingArtistListener(Long artistId, Long userId){
-        try {
-            if(!artistListenerRepo.existsByArtistIdAndUserId(artistId, userId)){
-                artistListenerRepo.save(new ArtistListenerEntity(artistId, userId));
-                return false;
-            }
-            return true;
-        } catch (DataIntegrityViolationException ignored) {
-            return true;
-        }
-    }
-
-    public void initializeNewUserArtistStatsOrIncrement(Long userId, Long artistId, String artistName){
-        UserArtistStatsId id = new UserArtistStatsId(userId, artistId);
-        userArtistStatsRepo.findById(id).ifPresentOrElse(
-                stats -> userArtistStatsRepo.incrementTotalPlays(id),
-                () -> userArtistStatsRepo.save(new UserArtistStatsEntity(id, artistName)) // just save, no increment
-        );
-    }
-
-    public void initializeNewUserTrackStatsOrIncrement(Long userId, Long trackId, String trackName, String artistName){
-        UserTrackStatsId id = new UserTrackStatsId(userId, trackId);
-        userTrackStatsRepository.findById(id).ifPresentOrElse(
-                stats -> userTrackStatsRepository.incrementTotalPlays(id),
-                () -> userTrackStatsRepository.save(new UserTrackStatsEntity(id, artistName, trackName))
-        );
-    }
 }
