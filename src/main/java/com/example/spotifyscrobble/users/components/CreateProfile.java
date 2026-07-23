14eb97bd84b42d39561bf12238d3cf5d3e65a2f0
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-07-22 20:11:54 -0400

    added component that makes sure a user profile is created if they dont already exist in the database due to third party creation

---
+++ src/main/java/com/example/spotifyscrobble/users/components/CreateProfile.java
@@ -0,0 +1,25 @@
+package com.example.spotifyscrobble.users.components;
+
+import com.example.spotifyscrobble.users.dto.UserProfileResponse;
+import com.example.spotifyscrobble.users.entity.UserEntity;
+import com.example.spotifyscrobble.users.repository.UserRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class CreateProfile {
+    private final UserRepository userRepo;
+
+    public CreateProfile(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public UserEntity getOrCreateProfile(Jwt jwt){
+        UUID userId = UUID.fromString(jwt.getSubject());
+        return userRepo.findById(userId).orElseGet(
+                () -> userRepo.save(new UserEntity(userId, "test", jwt.getClaimAsString("email")))
+        );
+    }
+}
