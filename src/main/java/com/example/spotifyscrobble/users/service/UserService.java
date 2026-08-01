7e3b63513babd230cbcc4281079f9ef6b703a7be
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-07-31 20:04:49 -0400

    made it to where createuserprofile is called from service and not directly from component class

--- src/main/java/com/example/spotifyscrobble/users/service/UserService.java
+++ src/main/java/com/example/spotifyscrobble/users/service/UserService.java
@@ -1,19 +1,29 @@
 package com.example.spotifyscrobble.users.service;
 
 import com.example.spotifyscrobble.users.UsersApi;
+import com.example.spotifyscrobble.users.components.CreateProfile;
 import com.example.spotifyscrobble.users.dto.UserRegisterRequest;
 import com.example.spotifyscrobble.users.repository.UserRepository;
 import com.example.spotifyscrobble.users.entity.UserEntity;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import com.example.spotifyscrobble.shared.UserAlreadyExistsException;
 
 @Service
 public class UserService implements UsersApi{
     private final UserRepository userRepo;
+    private final CreateProfile createProfile;
 
-    public UserService(UserRepository userRepo){
+    public UserService(UserRepository userRepo, CreateProfile createProfile){
         this.userRepo = userRepo;
+        this.createProfile = createProfile;
     }
+
+    public UserEntity createUserProfile(Jwt jwt){
+        return createProfile.getOrCreateProfile(jwt);
+    }
+
+
 
 }
