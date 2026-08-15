ce9d3d76cc9b585bbcbd6da3357398a5974399d6
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-08-15 19:39:35 -0400

    created public api usersapi for other modules to use to get user by username

--- src/main/java/com/example/spotifyscrobble/users/UsersApi.java
+++ src/main/java/com/example/spotifyscrobble/users/UsersApi.java
@@ -1,6 +1,6 @@
 package com.example.spotifyscrobble.users;
 
-import com.example.spotifyscrobble.users.entity.UserEntity;
-
 public interface UsersApi {
+
+    public GetUserByIdResponse getUserByUsername(String username);
 }
