d97ef3887b4506fe3b7829d7d9717d5ac90caa71
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-09-04 13:27:43 -0400

    Added new usersapi method and implemented it into user service

--- src/main/java/com/example/spotifyscrobble/users/UsersApi.java
+++ src/main/java/com/example/spotifyscrobble/users/UsersApi.java
@@ -1,6 +1,10 @@
 package com.example.spotifyscrobble.users;
 
+import java.util.UUID;
+
 public interface UsersApi {
 
     GetUserByIdResponse getUserByUsername(String username);
+
+    String getUsernameByUserId(UUID userId);
 }
