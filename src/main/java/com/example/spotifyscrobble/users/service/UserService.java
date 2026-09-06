7746a27bbe48e139284bf1ecfc0f1c4e8e760895
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-09-06 11:34:27 -0400

    added caching to getUsernameByUserId as it can be called multiple times in quick succession

--- src/main/java/com/example/spotifyscrobble/users/service/UserService.java
+++ src/main/java/com/example/spotifyscrobble/users/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.spotifyscrobble.users.components.CreateProfile;
 import com.example.spotifyscrobble.users.GetUserByIdResponse;
 import com.example.spotifyscrobble.users.repository.UserRepository;
 import com.example.spotifyscrobble.users.entity.UserEntity;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,7 @@ public class UserService implements UsersApi{
     }
 
     @Override
+    @Cacheable(cacheNames = "usernames", key = "#userId")
     public String getUsernameByUserId(UUID userId){
         UserEntity entity = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("hi")); // Need to create its own exception
         return entity.getUsername();
