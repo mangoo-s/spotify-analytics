3a244afc37dec11a166808c7dd9770158573bd72
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-09-10 18:40:40 -0400

    added getter

--- src/main/java/com/example/spotifyscrobble/statistics/entities/TrackListenerEntity.java
+++ src/main/java/com/example/spotifyscrobble/statistics/entities/TrackListenerEntity.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
 
 import java.util.UUID;
 @Entity
 @Table(name = "track_listeners", uniqueConstraints = {@UniqueConstraint(
         columnNames = {"trackId", "user_id"}
 )})
+@Getter
 public class TrackListenerEntity {
     @Id
     private Long trackId;
