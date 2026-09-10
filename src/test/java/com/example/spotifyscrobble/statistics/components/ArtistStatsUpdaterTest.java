413a3a10729e4fe13ee8b829c8593ac7bf0d9338
mangoo-s <254040549+mangoo-s@users.noreply.github.com>
2026-09-10 18:46:51 -0400

    Updated components to work with right types and added is exisintg listenere tests

--- src/test/java/com/example/spotifyscrobble/statistics/components/ArtistStatsUpdaterTest.java
+++ src/test/java/com/example/spotifyscrobble/statistics/components/ArtistStatsUpdaterTest.java
@@ -1,6 +1,5 @@
 package com.example.spotifyscrobble.statistics.components;
 
-import com.example.spotifyscrobble.statistics.entities.ArtistListenerEntity;
 import com.example.spotifyscrobble.statistics.entities.ArtistStatsEntity;
 import com.example.spotifyscrobble.statistics.repositories.ArtistListenerRepository;
 import com.example.spotifyscrobble.statistics.repositories.ArtistStatsRepository;
@@ -29,10 +28,12 @@ public class ArtistStatsUpdaterTest {
     @InjectMocks
     private ArtistStatsUpdater artistStatsUpdater;
 
+    @Mock
+    private IsExistingListener isExistingListener;
+
     @Test
     void recordListen_DoesNotCreateArtistWhenArtistAlreadyExists(){
         when(artistStatsRepo.existsById(1L)).thenReturn(true);
-        when(artistListenerRepo.existsByArtistIdAndUserId(1L, UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"))).thenReturn(true);
 
         artistStatsUpdater.recordListen(1L, "Radiohead", UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"));
 
@@ -42,39 +43,35 @@ public class ArtistStatsUpdaterTest {
     @Test
     void recordListen_CreatesNewArtistWhenArtistDoesNotAlreadyExist(){
         when(artistStatsRepo.existsById(1L)).thenReturn(false);
-        when(artistListenerRepo.existsByArtistIdAndUserId(1L, UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"))).thenReturn(true);
 
         artistStatsUpdater.recordListen(1L, "Radiohead", UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"));
         ArgumentCaptor<ArtistStatsEntity> captor = ArgumentCaptor.forClass(ArtistStatsEntity.class);
         verify(artistStatsRepo).save(captor.capture());
         assertThat(captor.getValue().getArtistId()).isEqualTo(1L);
-        assertThat(captor.getValue().getName()).isEqualTo("RadioHead");
+        assertThat(captor.getValue().getName()).isEqualTo("Radiohead");
     }
 
     @Test
     void recordListen_IncrementsListenersForNewListener(){
         when(artistStatsRepo.existsById(1L)).thenReturn(true);
-        when(artistListenerRepo.existsByArtistIdAndUserId(1L, UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"))).thenReturn(false);
-        ArgumentCaptor<ArtistListenerEntity> captor = ArgumentCaptor.forClass(ArtistListenerEntity.class);
+        when(isExistingListener.isExistingArtistListener(UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"), 1L)).thenReturn(false);
 
         artistStatsUpdater.recordListen(1L, "Radiohead", UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"));
 
-        verify(artistListenerRepo).save(captor.capture());
-        assertThat(captor.getValue().getArtistId()).isEqualTo(1L);
-        assertThat(captor.getValue().getUserId()).isEqualTo(UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"));
         verify(artistStatsRepo).incrementListeners(1L);
     }
 
     @Test
     void recordListen_DoesNotIncrementListenersForReturningListener(){
         when(artistStatsRepo.existsById(1L)).thenReturn(true);
-        when(artistListenerRepo.existsByArtistIdAndUserId(1L, UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"))).thenReturn(true);
+        when(isExistingListener.isExistingArtistListener(UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"), 1L)).thenReturn(true);
 
         artistStatsUpdater.recordListen(1L, "Radiohead", UUID.fromString("52a40a30-c59e-40a2-b92e-5417b0c3a31b"));
 
         verify(artistListenerRepo, never()).save(any());
         verify(artistStatsRepo, never()).incrementListeners(anyLong());
     }
+    
 
     @Test
     void recordListen_DoesNotIncrementListenersWhenRaceConditionOccursOnSave(){
